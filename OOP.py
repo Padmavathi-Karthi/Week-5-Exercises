6d0f576f385ba18488225d32838e23df232abe69
Padmavathi-Karthi <padhukarthi17@gmail.com>
2023-04-20 10:40:20 +0100

    oop script added

---
+++ OOP.py
@@ -0,0 +1,2 @@
+# 1. Creating new instances of class objects can be a great way to keep track of values using attributes associated with the object. The values of these attributes can be easily changed at the object level.  The following code illustrates a famous quote by George Bernard Shaw, using objects to represent people. Fill in the blanks to make the code satisfy the behavior described in the quote. 
+
